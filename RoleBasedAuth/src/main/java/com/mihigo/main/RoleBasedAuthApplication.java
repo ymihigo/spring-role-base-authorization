f21d22cecca5fdf45b8e4b3780e558f49c5fd260
MIHIGO Yves <ymihigo70@gmail.com>
2022-04-14 11:03:44 +0200

    initial

---
+++ RoleBasedAuth/src/main/java/com/mihigo/main/RoleBasedAuthApplication.java
@@ -0,0 +1,13 @@
+package com.mihigo.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoleBasedAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoleBasedAuthApplication.class, args);
+	}
+
+}
